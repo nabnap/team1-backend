edd08d9a4daa6c0ff7f7103c4f489e89c429a79c
root <nicholasabiegel@lewisu.edu>
2018-11-28 18:14:17 +0000

    PSR4 and Config

--- src/public/index.php
+++ src/public/index.php
@@ -4,7 +4,14 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../../vendor/autoload.php';
 
-$app = new \Slim\App;
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+$config['db']['host'] = '192.34.63.80';
+$config['db']['user'] = '';
+$config['db']['pass'] = '';
+$config['db']['dbname'] = 'yetube';
+$app = new \Slim\App(['settings' => $config]);
+
 $app->get('/hello/{name}', function(Request $request, Response $response, array $args){
   $name = $args['name'];
   $response->getBody()->write("Hello, $name");
