41b95a82a5a1e0270e5a758c7e285caf039ce8cd
root <nicholasabiegel@lewisu.edu>
2018-11-28 18:30:11 +0000

    BS changes

--- src/public/index.php
+++ src/public/index.php
@@ -14,10 +14,20 @@ $app = new \Slim\App(['settings' => $config]);
 $container = $app->getContainer();
 $container['logger'] = function($c){
   $logger = new \Monolog\Logger('my_logger');
-  $file_handler = new \Monolog\Handler\StreamHandler('../../logs/app.log');
+  $file_handler = new \Monolog\Handler\StreamHandler('../logs/app.log');
   $logger->pushHandler($file_handler);
   return $logger;
 };
+
+$container['db'] = function ($c) {
+    $db = $c['settings']['db'];
+    $pdo = new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],
+        $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
 $app->get('/hello/{name}', function(Request $request, Response $response, array $args){
   $name = $args['name'];
   $response->getBody()->write("Hello, $name");
