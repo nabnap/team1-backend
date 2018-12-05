a12abb169291474c0bee76169e1fa42e6f1f5135
Nicholas <8355085+strikerforce@users.noreply.github.com>
2018-12-04 20:09:52 -0600

    Ratings controller

--- src/app/Controllers/RatingController.php
+++ src/app/Controllers/RatingController.php
@@ -13,10 +13,10 @@ class RatingController
     public function post($request, $response, $args) {
       $auth0_id = $request->getParam('authId');
       $video_id = $request->getParam('videoId');
-      $liked = request->getParam('liked');
-      //$sql = "INSERT INTO ratings (video_id, user_id, liked) VALUES (?,(SELECT user_id FROM users WHERE auth0_id = ?),?) ON DUPLICATE KEY UPDATE liked = ?";
-      //$stmt = $this->container->db->prepare($sql);
-      //$stmt->execute([$video_id, $auth0_id, $liked, $liked]);
+      $liked = $request->getParam('liked');
+      $sql = "INSERT INTO ratings (video_id, user_id, liked) VALUES (?,(SELECT user_id FROM users WHERE auth0_id = ?),?) ON DUPLICATE KEY UPDATE liked = ?";
+      $stmt = $this->container->db->prepare($sql);
+      $stmt->execute([$video_id, $auth0_id, $liked, $liked]);
       return $response;
     }
 }
