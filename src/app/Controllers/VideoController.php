2d75bd8093f1f1b1cd2cf5600b70f1c2019e6347
Nicholas <8355085+strikerforce@users.noreply.github.com>
2018-12-04 19:35:47 -0600

    correct videos controller

--- src/app/Controllers/VideoController.php
+++ src/app/Controllers/VideoController.php
@@ -25,9 +25,9 @@ class VideoController
     
     public function get($request, $response, $args) {
       $video_id = $args['id'];
-      $sql = "SELECT videos.video_id,username,title,description,video_src,views ";
-      $sql .= "FROM videos JOIN users ON users.user_id = videos.user_id LEFT JOIN ratings ON ratings.video_id = videos.video_id";
-      $sql .= "WHERE video_id = ? AND loaded = 1 GROUP videos.video_id LIMIT 1";
+      $sql = "SELECT videos.video_id,username,title,description,video_src,views, SUM(liked) AS likes, COUNT(liked) AS total ";
+      $sql .= "FROM videos JOIN users ON users.user_id = videos.user_id LEFT JOIN ratings ON ratings.video_id = videos.video_id ";
+      $sql .= "WHERE video_id = ? AND loaded = 1 GROUP BY videos.video_id LIMIT 1";
       $stmt = $this->container->db->prepare($sql);
       if($stmt->execute([$video_id])){
         $data = $stmt->fetch();
